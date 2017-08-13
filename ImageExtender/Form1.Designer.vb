01b8185373fc155fa4b13313c3f01136031d613a
pmx <mingl0280@gmail.com>
2017-08-12 22:58:54 -0600

    Init commit
    Signed-off-by: pmx <mingl0280@gmail.com>

--- ImageExtender/Form1.Designer.vb
+++ ImageExtender/Form1.Designer.vb
@@ -22,9 +22,119 @@ Partial Class Form1
     '不要使用代码编辑器修改它。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "文件"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(47, 6)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(709, 21)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(762, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "浏览"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(948, 33)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "开始扩大"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar1.Location = New System.Drawing.Point(47, 33)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(895, 23)
+        Me.ProgressBar1.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 12)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "进度"
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(843, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 12)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "放大倍率"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"2", "4", "8", "16", "32", "64"})
+        Me.ComboBox1.Location = New System.Drawing.Point(902, 6)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 20)
+        Me.ComboBox1.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1035, 62)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox1 As ComboBox
 End Class
--- ImageExtender/Form1.vb
+++ ImageExtender/Form1.vb
@@ -1,3 +1,74 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.Drawing
+Imports System.Threading
 
+Public Class Form1
+
+    Private Delegate Sub SetProgressBarMaxD(ByRef i As Integer)
+    Private Delegate Sub NoParamD()
+
+    Private ProcessThread As Thread
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim OFDialg As New OpenFileDialog()
+        OFDialg.Filter = "图像文件|*.bmp;*.jpg;*.gpeg;*.png;*.tif;*.tiff"
+        OFDialg.Multiselect = False
+        Dim OFRet = OFDialg.ShowDialog()
+        If (OFRet = DialogResult.OK) Then
+            TextBox1.Text = OFDialg.FileName
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Button2.Enabled = False
+        ProcessThread = New Thread(AddressOf ImageExtender)
+        ProcessThread.Start(Val(ComboBox1.SelectedItem))
+    End Sub
+
+    Private Sub SetProgressBarMax(ByRef i As Integer)
+        ProgressBar1.Maximum = i
+        ProgressBar1.Step = 1
+        ProgressBar1.Value = 0
+    End Sub
+    Private Sub ProgressBarDoProgress()
+        ProgressBar1.PerformStep()
+    End Sub
+
+    Private Sub ProcessFinish()
+        Button2.Enabled = True
+        MessageBox.Show("扩大完成", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    ''' <summary>
+    ''' 用了简单低效的方法，反正放大两张图片而已。
+    ''' </summary>
+    ''' <param name="MagT"></param>
+    Private Sub ImageExtender(MagT)
+        Dim iFile As New FileInfo(TextBox1.Text)
+        Dim MagTimes = CInt(MagT)
+        Debug.WriteLine(iFile.FullName)
+        Dim nFilename As String = iFile.FullName.Replace(iFile.Extension, "-extended" + iFile.Extension)
+        If (File.Exists(nFilename)) Then
+            nFilename = nFilename.Replace(iFile.Extension, "-" + MagTimes.ToString() + "x-" + Now.ToFileTimeUtc().ToString() + iFile.Extension)
+        End If
+        Dim OrigBitmap As Bitmap = Image.FromFile(iFile.FullName)
+        BeginInvoke(New SetProgressBarMaxD(AddressOf SetProgressBarMax), OrigBitmap.Width)
+        Dim NewBitmap = New Bitmap(OrigBitmap.Width * MagTimes, OrigBitmap.Height * MagTimes)
+        For x = 0 To OrigBitmap.Width - 1
+            For y = 0 To OrigBitmap.Height - 1
+                For x1 = 0 To MagTimes - 1
+                    For y1 = 0 To MagTimes - 1
+                        NewBitmap.SetPixel(x * MagTimes + x1, y * MagTimes + y1, OrigBitmap.GetPixel(x, y))
+                    Next
+                Next
+            Next
+            BeginInvoke(New NoParamD(AddressOf ProgressBarDoProgress))
+        Next
+        NewBitmap.Save(nFilename, OrigBitmap.RawFormat)
+        BeginInvoke(New NoParamD(AddressOf ProcessFinish))
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.SelectedIndex = 1
+    End Sub
 End Class
